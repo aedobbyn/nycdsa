49ad1252bb50c84c8d9f0aa5542bd36fdf41c0c5
aedobbyn <adobbs11@gmail.com>
2018-02-05 12:21:05 -0800

    implement the purrr solution in a function

--- palindromes_vectorized.R
+++ palindromes_vectorized.R
@@ -1,9 +1,7 @@
+library(tidyverse)
 
 
 check_equal <- function(obj) {
-  obj <- obj %>% as.character() %>% 
-    stringr::str_extract_all('[0-9]', simplify = TRUE) %>%  # Remove decimals and negative signs if they exist
-    stringr::str_split("") %>% purrr::as_vector()
   len <- length(obj)
   if (len %% 2 == 0) {    
     lefts <- 1:(len/2)
@@ -20,51 +18,7 @@ check_equal <- function(obj) {
   }
 }
 
-
-check_equal <- function(obj) {
-
-  len <- nchar(obj)
-  if (len %% 2 == 0) {    
-    lefts <- 1:(len/2)
-    rights <- len:(max(lefts) + 1)    
-  } else {                              # If we have an odd number of digits, skip the middle one
-    lefts <- floor(1:(len/2))
-    rights <- len:(max(lefts) + 2)
-  }
-  
-  if (all(
-    obj %>% substr(min(lefts), max(lefts)) == obj %>% substr(min(rights), max(rights))
-    # obj[lefts] == obj[rights])
-    )) {  # Check equality    
-    return(TRUE)
-  } else {
-    return(FALSE)
-  }
-}
-
-
-one <- 999:100
-two <- 999:100
-
-# mult <- one*two 
-mult_df <- expand.grid(one, two) %>% 
-  mutate(
-    mult = Var1 * Var2
-  ) %>% as_tibble() %>% 
-  arrange(desc(Var1, Var2))
-
-
-
-mult_df %>% 
-  mutate(
-    palindrome = map_lgl(mult, check_equal_2)
-  ) %>% 
-  filter(palindrome == TRUE) %>% 
-  slice(1:1)
-
-
-foo <- mult_df[1:10, ]
-
+# Helper to turn a number into its individual digits
 stringify <- function(e) {
   e <- e %>% as.character() %>% 
     stringr::str_extract_all('[0-9]', simplify = TRUE) %>%  # Remove decimals and negative signs if they exist
@@ -72,8 +26,61 @@ stringify <- function(e) {
   return(e)
 }
 
-bar <- foo %>% mutate(
-  one_as_str = Var1 %>% map(stringify) %>% list()
-  # two_as_str = Var2 %>% stringify() %>% nest()
-)
+stringify(123)
+# "1" "2" "3"
+
+
+# Set our 
+one <- 999:100
+two <- 999:100
+
+# Expand one and two into all their possible combinations
+mult_df <- expand.grid(one, two) %>% 
+  mutate(
+    mult = Var1 * Var2
+  ) %>% as_tibble() %>% 
+  arrange(desc(Var1, Var2))
+
+mult_df_trues <- mult_df %>% 
+  filter(mult == 698896) %>% 
+  rowwise() %>%
+  mutate(
+    mult_as_str = mult %>% stringify() %>% list()
+  ) 
+
+mult_df_trues %>% 
+  mutate(
+    palindrome = check_equal(mult_as_str)
+  ) %>% 
+  filter(palindrome == TRUE) %>% 
+  slice(1:1)
+
+
+find_palindromes <- function(start = 999, end = 100) {
+  a <- start:end
+  b <- start:end
+  
+  # Expand one and two into all their possible combinations
+  mult_df <- expand.grid(a, b) %>% 
+    as_tibble() %>% 
+    mutate(
+      mult = Var1 * Var2
+    ) %>% 
+    arrange(desc(Var1, Var2))
+  
+  out <- mult_df %>%
+    rowwise() %>%
+    mutate(
+      mult_as_str = mult %>% stringify() %>% list(),
+      palindrome = mult_as_str %>% check_equal()
+    ) %>% 
+    filter(palindrome == TRUE) %>% 
+    slice(1:1)
+  
+  return(out)
+}
+
+sol <- find_palindromes()
+
+
 

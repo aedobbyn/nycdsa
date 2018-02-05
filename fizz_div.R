be3dd1e4023ab6cf46dbf8f1e22745da1f4dc4d7
aedobbyn <adobbs11@gmail.com>
2018-02-04 23:22:59 -0800

    fix typo in fizz_div()

--- fizz_div.R
+++ fizz_div.R
@@ -1,13 +1,13 @@
 
 ### Find the sum of all numbers below 1000 that are divisible by 3 or 5 ###
 
-# Assuming numbers must be positive and whole (so decrement step is 1)
+# Assuming numbers must be positive
 
 fizz_div <- function(decrement = 1) {
   sum <- 0 
   x <- 1000 - decrement
   while (x > 0) {
-    if (x %% 3 | x %% 5) {
+    if (x %% 3 == 0 | x %% 5 == 0) {
       sum <- sum + x
     }
     x <- x - decrement
@@ -16,5 +16,5 @@ fizz_div <- function(decrement = 1) {
 }
 
 fizz_div()
-# 466335
+# 233168
 

c22aa0bd6b7cba65c82aa7b6db5befb3df337676
aedobbyn <adobbs11@gmail.com>
2018-02-05 14:57:17 -0800

    batch find_palindromes

--- palindromes_vectorized.R
+++ palindromes_vectorized.R
@@ -18,6 +18,23 @@ check_equal <- function(obj) {
   }
 }
 
+check_equal_break <- function(vec) {
+  len <- length(obj)
+  if (len %% 2 == 0) {    
+    lefts <- 1:(len/2)
+    rights <- len:(max(lefts) + 1)    
+  } else {                              # If we have an odd number of digits, skip the middle one
+    lefts <- floor(1:(len/2))
+    rights <- len:(max(lefts) + 2)
+  }
+  
+  if (all(obj[lefts] == obj[rights])) {  # Check equality    
+    return(TRUE)
+  } else {
+    return(FALSE)
+  }
+}
+
 # Helper to turn a number into its individual digits
 stringify <- function(e) {
   e <- e %>% as.character() %>% 
@@ -48,6 +65,8 @@ mult_df_trues <- mult_df %>%
     mult_as_str = mult %>% stringify() %>% list()
   ) 
 
+mult_df_trues$mult_as_str %>% map(check_equal)
+
 mult_df_trues %>% 
   mutate(
     palindrome = check_equal(mult_as_str)
@@ -56,7 +75,7 @@ mult_df_trues %>%
   slice(1:1)
 
 
-find_palindromes <- function(start = 999, end = 100) {
+find_palindromes <- function(start = 999, end = 100, batch_percent = 0.005) {
   a <- start:end
   b <- start:end
   
@@ -66,21 +85,35 @@ find_palindromes <- function(start = 999, end = 100) {
     mutate(
       mult = Var1 * Var2
     ) %>% 
-    arrange(desc(Var1, Var2))
+    arrange(desc(mult))
   
-  out <- mult_df %>%
-    rowwise() %>%
-    mutate(
-      mult_as_str = mult %>% stringify() %>% list(),
-      palindrome = mult_as_str %>% check_equal()
-    ) %>% 
-    filter(palindrome == TRUE) %>% 
-    filter(mult == max(mult))
+  n_per_batch <- (nrow(mult_df)*batch_percent) %>% floor()
+  batch_start <- 1
+  batch_end <- n_per_batch
+  mult_df_batch_trues <- tibble()
+    
+  while (nrow(mult_df_batch_trues) == 0) {
+    mult_df_batch_trues <- mult_df %>%
+      slice(batch_start:batch_end) %>% 
+      rowwise() %>%
+      mutate(
+        mult_as_str = mult %>% stringify() %>% list(),
+        palindrome = mult_as_str %>% check_equal()
+      ) %>% 
+      filter(palindrome == TRUE) %>% 
+      filter(mult == max(mult))
+    
+    batch_start <- batch_end
+    batch_end <- batch_end + n_per_batch
+      
+  }
   
-  return(out)
+  return(mult_df_batch_trues)
 }
 
-sol <- find_palindromes()
+find_palindromes()
 
+sol <- find_palindromes()
+sol %>% pluck()
 
 

2e3e52ced9f55e548f9b585c582bc4a263ac336b
aedobbyn <adobbs11@gmail.com>
2018-02-08 15:06:50 -0800

    finish app questions

--- fizz_div.R
+++ fizz_div.R
@@ -1,16 +1,25 @@
 
 ### Find the sum of all numbers below 1000 that are divisible by 3 or 5 ###
 
-# Assuming numbers must be positive
+# Here assuming numbers must be positive
 
-fizz_div_while <- function(decrement = 1) {
+library(tidyverse)
+
+#### While loop approach ####
+
+fizz_div_while <- function(min = 0, max = 1000, by = 1, gte = FALSE) {
+  
+  # Should max be inclusive or exclusive?
+  if (gte == FALSE) {
+    max <- max - by
+  }
+  x <- max
   sum <- 0 
-  x <- 1000 - decrement
-  while (x > 0) {
+  while (x > min) {
     if (x %% 3 == 0 | x %% 5 == 0) {
       sum <- sum + x
     }
-    x <- x - decrement
+    x <- x - by
   }
   return(sum)
 }
@@ -19,8 +28,9 @@ fizz_div_while()
 # 233168
 
 
+##### Vectorized approach #####
 
-
+# Helper for determining whether a single number is divisible by 3 or 5
 keep_it <- function(e) {
   if (e %% 3 == 0 || e %% 5 == 0)  {
     out <- TRUE
@@ -67,11 +77,15 @@ fizz_div_vec()
 # 233168
 
 
+# ---- Compare runtimes -----
+
 system.time(fizz_div_while())
+# user  system elapsed 
+# 0.001   0.000   0.001 
 system.time(fizz_div_vec())
+# user  system elapsed 
+# 0.046   0.001   0.047 
+# ^^^ 0.03 seconds faster without the intermediate assignment to variables num_space
 
-
-
-
-
+# Given all this, I'd go with fizz_div_while()
 
